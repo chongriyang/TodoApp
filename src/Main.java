536f9e726b1d06f3c402a161df465aa16ea9f55a
issuser <afei.yyf@gmail.com>
2019-03-08 14:14:52 +0800

    create main class

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println(">");
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			String input = "";
+			if (sc.hasNext()) {
+				input = sc.next();
+				System.out.println(input);
+			}
+		}
+	}
+}

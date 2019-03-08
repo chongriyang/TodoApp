a54cc49f820ff9949ef83d36d02eb8e4c51260dd
issuser <afei.yyf@gmail.com>
2019-03-08 14:19:46 +0800

    create task object

---
+++ src/Task.java
@@ -0,0 +1,34 @@
+import java.util.Date;
+
+public class Task {
+	String name;
+	String description;
+	Date startDate;
+	Date endDate;
+
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+}
